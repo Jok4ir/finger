366e613316b251e5c41de6324fd6c5c096bbc8ae
Setra Solofoniaina <60129070+Setra-Solofoniaina@users.noreply.github.com>
2021-02-25 16:05:28 +0300

    faire un commit du changement

--- fingerprint/main.py
+++ fingerprint/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_restful import Resource, Api, reqparse
+from flask_restful import R blablabalesource, Api, reqparse
 import werkzeug
 from werkzeug.utils import secure_filename
 import os
@@ -22,7 +22,7 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-### this must be commited
+### this must be commited modificationfdsfsqfqsdf
 parser = reqparse.RequestParser()
 parser.add_argument('file',type=werkzeug.datastructures.FileStorage, location='files')
 
@@ -88,7 +88,7 @@ def scan():
     # time.sleep(1)        
     print("scaning")
     # ser.write(bytes('scan', 'UTF-8'))
-    toreturn = -1;
+    toreturn = -1
     while True:
         try:
             a = ser.readline().decode('UTF-8').strip()
